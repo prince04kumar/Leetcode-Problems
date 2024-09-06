476cdc642f4c4447d324304bf03b77e9be14fa8c
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2024-09-06 11:01:36 +0530

    Time: 89 ms (40.45%), Space: 96.3 MB (25.41%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxProfit(vector<int>& arr) {
+        #define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+
         int n = arr.size();
         int buyPrice = arr[0];
         int profit = 0;
