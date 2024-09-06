36e124b1aeb55a74edb7fd5c92d706cb7eb8a4ed
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2024-09-06 19:11:40 +0530

    Time: 7 ms (96.52%), Space: 20.3 MB (97.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& arr) {
-        int size = arr.size();
-        int start = 0;
-        int end = arr.size()-1;
-        int mid = start+ (end - start )/2;
-
-        while(start<=end){
-            if(arr[mid+1]-arr[mid]!=1){
-                return arr[mid+1];
-            }
-            else if (arr[mid]-mid==0){
-                start = mid +1;
-               mid = start+ (end - start )/2;
-
-            }
-            else if (arr[mid]-mid==1){
-                end
-            }
+    int missingNumber(vector<int>& nums) {
+        
+        int sum1= 0;
+        int sum2 = 0;
+        int missingNum = -1;
+        for (int i = 0 ; i<=nums.size(); i ++){
+            sum1 = sum1+i;
         }
-
+        for(int j = 0 ; j< nums.size(); j++){
+            sum2 = sum2 + nums[j];
+        }
+missingNum = sum1 - sum2;
+return missingNum;
     }
+
 };
