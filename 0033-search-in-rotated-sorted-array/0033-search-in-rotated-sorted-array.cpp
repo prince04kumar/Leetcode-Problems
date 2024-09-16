524a19e041a9192063022c02c7f7c0522d5006fa
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2024-09-16 16:44:15 +0530

    Time: 0 ms (100%), Space: 14.1 MB (48.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,69 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
+    int getPivotIndex(vector<int>& arr) {
         int s = 0;
-        int e = n - 1;
-        int mid = s + (e - s) / 2;
-        int ans = -1;
-        while (s <= e) {
-            if (nums[mid] == target) {
-                ans = mid;
-            }
-            if (nums[mid] >= nums[s]) {
-                if (target >= nums[s] && target <= nums[mid]) {
-                    e = mid - 1;
-                } else {
-                    s = mid + 1;
-                }
+        int n = arr.size();
+        int e = n-1;
+        int mid = s+(e-s)/2;
 
-            } else {
-                if (nums[mid] <= target && target <= nums[e]) {
-                    s = mid + 1;
-                } else {
-                    e = mid - 1;
-                }
+        while(s <= e) {
+            if(s == e) {
+                //single element k case me 
+                //humne usse hi pivot maaanlia hai
+                return s;
             }
-            mid = s + (e - s) / 2;
+            if(mid+1 < n && arr[mid] > arr[mid+1]) {
+                return mid;
+            }
+            //kab left or kab right
+            if(arr[s] > arr[mid] ) {
+                e = mid-1;
+            }
+            else {
+                //right
+                s = mid+1;
+            }
+            mid = s+(e-s)/2;
         }
-        return ans;
+        return -1;
+    }
+    int binarySearch(int s, int e, int target, vector<int> arr) {
+        int mid = s+(e-s)/2;
+        while(s<=e) {
+            if(arr[mid] == target) {
+                return mid;
+            }
+            if(target > arr[mid]) {
+                s = mid+1;
+            }
+            else {
+                e = mid-1;
+            }
+            mid = s+(e-s)/2;
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        //Step1
+        int pivotIndex = getPivotIndex(nums);
+
+        //lineA
+        //start -> 0 index
+        //end -> pivotIndex
+
+        //line B
+        //start -> pivotIndex+1;
+        //end -> n-1;
+        int n = nums.size();
+        if(target >= nums[0] && target <= nums[pivotIndex]) {
+            int ans = binarySearch(0, pivotIndex, target, nums);
+            return ans;
+        }
+        else {
+            int ans = binarySearch(pivotIndex+1, n-1, target, nums);
+            return ans;
+        }
+        return -1;
+         
     }
 };
