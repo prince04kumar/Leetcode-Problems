c1aaa8695efb2b3571cd2e46aab207dbf8cae01d
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2024-09-23 01:18:40 +0530

    Time: 15 ms (84.08%), Space: 25 MB (11.12%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0;
-        if (nums.size()==1){
-            ans = nums[0];
-        }
-        else if(nums[0]!=nums[1]){
-            ans = nums[0];
-        }
-        else {
-            for (int i = 1; i < nums.size(); i++) {
-            if ((i + 1) <= nums.size() && nums[i] != nums[i + 1] &&
-                nums[i] != nums[i - 1]) {
-                ans = nums[i];
-                break;
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+
+        int s = 0;
+        int e = n - 1;
+        int mid = s + (e - s) / 2;
+        while (s <= e) {
+            if (mid - 1 > 0 && mid + 1 < n && nums[mid] - nums[mid - 1] > 0 &&
+                nums[mid + 1] - nums[mid] > 0)
+                return nums[mid];
+            if (mid - 1 > 0 && mid + 1 < n &&(mid % 2 == 0 && nums[mid] == nums[mid + 1]) ||
+                mid % 2 == 1 && nums[mid] == nums[mid - 1]) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
             }
-            }
+            mid = s + (e - s) / 2;
         }
-        return ans;
+        return 0;
     }
 };
