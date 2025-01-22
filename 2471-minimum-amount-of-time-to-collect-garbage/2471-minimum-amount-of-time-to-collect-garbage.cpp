a9c3232bbe181838334c68eb76a3476323e1f489
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-01-22 09:59:32 +0530

    Time: 76 ms (5.01%), Space: 123.2 MB (5.47%) - LeetHub

--- 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
-int traveling(int ind, int i , vector<int>& travel){
-    int traveling_cost = 0;
-    if(ind == 0){
-        return 0;
-    }
-    else{
-        for (int i = i-1 ; i>=ind ; i-- ){
-            traveling_cost += travel[i];
-        }
-    }
-    return traveling_cost;
-}
+
    vector<int> garbageCount(const string& s) {
-        vector<int> count(3, 0); // Count for G, M, P
+        vector<int> count(3, 0); 
         for (char c : s) {
             if (c == 'G') count[0]++;
             else if (c == 'M') count[1]++;
@@ -29,12 +18,11 @@ int traveling(int ind, int i , vector<int>& travel){
         int totalGarbage = 0;
         vector<int> cumulativeTravel(length, 0);
 
-        // Precompute cumulative travel times
+     
         for (int i = 1; i < length; i++) {
             cumulativeTravel[i] = cumulativeTravel[i - 1] + travel[i - 1];
         }
 
-        // Calculate total garbage and travel costs
         for (int i = 0; i < length; i++) {
             vector<int> counts = garbageCount(garbage[i]);
 
