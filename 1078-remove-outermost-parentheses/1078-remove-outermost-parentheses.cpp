4d96716a652d0ff5d4f57239afc12d8dfd8accc7
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-01-24 00:30:15 +0530

    Time: 0 ms (100%), Space: 9 MB (27.2%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-     int size = s.size();
-     string ans;
-     int bal = 0;
-     for(int i = 0; i<size ; i++){
-        if(s[i]=='('){
-            if(bal>0){
-                ans+=s[i];
+        string st; 
+        int open = 0;
+        int close = 0;
+        int s_in = -1;
+        
+        for(int i=0; i < s.size(); i++)
+        {
+            if(s[i]=='(')
+            {
+                if(s_in==-1)
+                    s_in = i;
+                open++;
             }
-            bal++;
-
-        }
-        else{
-            bal--;
-            if(bal>0){
-                ans+=s[i];
+            if(s[i]==')')
+            {
+                close++;
             }
-
+            
+            if(open==close&&open==1)
+            {
+                open = 0;
+                close = 0;
+            }
+            
+            if(open==close&&open!=1)
+            {
+                //cout << s_in+1 << i-1 << "\n";
+                   
+                    //st += s.substr(s_in+1, i-1);
+                
+                for(int j = s_in+1; j < i; j++)
+                    st += s[j];
+                //cout << st << "\n";
+                s_in = -1;
+                open = 0;
+                close = 0;
+            }
         }
-     }
-     return ans;
+        /*if(st[st.size()-1]==st[st.size()-2])
+            st = st.substr(0, st.size()-1);*/
+        return st;
+        
     }
 };
