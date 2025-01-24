bd8c21bf636138d00263d04a53047b40b81608ee
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-01-24 09:46:04 +0530

    Time: 4 ms (43.52%), Space: 9.7 MB (48.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
-    // bool isFind(string &s , char t){
-    //     auto it = find(s.begin(),s.end(),t);
-    // bool ans = true;
-    // if (it != s.end()) {
-    //    ans = true;
-    // } else {
-    //    ans = false;
-    // }
-    // return ans;
-    // }
+  
     bool isAnagram(string s, string t) {
-        // bool ans = true;
+      
         if(t.size()<s.size() || t.size()>s.size()){
             return false;
         }
-        // for(int i = 0 ; i < t.size(); i++){
-        //     if(!isFind(s,t[i])){
-        //         return false;
-        //     }
-        // }
+     
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         for(int i = 0 ; i< s.size(); i++){
