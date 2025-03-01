b5c001f80bf61e2582fccb9eec3aa74ee48ec370
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-03-01 11:24:26 +0530

    Time: 3 ms (89.82%), Space: 24 MB (35.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& a) {
-       int size = a.size();
-     
-    sort(a.begin(), a.end() , [](const vector<int>&x , const vector<int>&y) {
-        if(x[0]==y[0])
-        return x[1] < y[1];
-        else {
-           return x[0]<y[0];
-        }
-    });
-    
-
-
-    vector<vector<int>>answer;
-    answer.push_back(a[0]);
-    int j = 0;
-
-    for(int i = 1 ; i< a.size(); i++){
-    vector<int>check = a[i];
-    if(answer[j][1] >= check[0]){
-    if(answer[j][1] >= check[1]) 
-        continue;
-        else{
-    answer[j][1] = check[1];
-        }
-    }
-    else{
-        answer.push_back(check);
-        j++;
-    }
-
+        if (a.empty()) return {};
         
-    }
-    return answer; 
+        // Sort intervals based on the start time
+        sort(a.begin(), a.end());
+
+        vector<vector<int>> answer;
+        answer.push_back(a[0]);
+
+        for (int i = 1; i < a.size(); i++) {
+            if (answer.back()[1] >= a[i][0]) {
+               
+                answer.back()[1] = max(answer.back()[1], a[i][1]);
+            } else {
+                
+                answer.push_back(a[i]);
+            }
+        }
+        return answer;
     }
 };
