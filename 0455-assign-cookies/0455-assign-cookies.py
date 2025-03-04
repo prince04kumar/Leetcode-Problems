75ddb409012980c7971f4b75dc83d189ccf12c7d
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-03-05 00:16:23 +0530

    Time: 43 ms (8.7%), Space: 14.2 MB (42.71%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+       g.sort()
+       s.sort()
+
+       i =0
+       j = 0
+       count = 0
+       while i<len(g) and j < len(s):
+            if(g[i]<=s[j]):
+                count += 1
+                i += 1
+                j += 1
+            else :
+                j +=1
+       return count         
+   
+      
