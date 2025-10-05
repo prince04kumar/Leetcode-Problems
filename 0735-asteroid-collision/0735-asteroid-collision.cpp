07bd97c53a6dfc9105dd405356c4f3e76d19cbcf
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-10-05 18:55:00 +0530

    Time: 3 ms (29.65%), Space: 22.4 MB (5.16%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,46 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         int n = asteroids.size();
         stack<int> st;
         vector<int> ans;
+
         for (int i = 0; i < n; i++) {
             if (st.empty()) {
                 st.push(asteroids[i]);
             } else {
                 if (st.top() > 0 && asteroids[i] > 0) {
                     st.push(asteroids[i]);
-                } else if (st.top() > 0 && asteroids[i] < 0) {
-                    if (abs(st.top()) > abs(asteroids[i])) {
-                        continue;
-                    } else if (abs(st.top()) < abs(asteroids[i])) {
-                        while (!st.empty() && st.top() > 0 &&
-                               asteroids[i] < 0 &&
-                               (abs(st.top()) < abs(asteroids[i]))) {
-                            st.pop();
-                        }
-                        if (abs(st.top()) == abs(asteroids[i])) {
-                            st.pop();
-                            continue;
-                        }else if(abs(st.top()) > abs(asteroids[i])){
-                            continue;
-                        }
-                         else {
-                            st.push(asteroids[i]);
-                        }
-                    }
-                    else{
+                } 
+                else if (st.top() > 0 && asteroids[i] < 0) {
+                    // collision case
+                    bool destroyed = false;
+                    while (!st.empty() && st.top() > 0 && abs(st.top()) < abs(asteroids[i])) {
                         st.pop();
-                        continue;
                     }
-                }
 
-                else {
+                    if (!st.empty() && st.top() > 0 && abs(st.top()) == abs(asteroids[i])) {
+                        st.pop();
+                        destroyed = true;
+                    }
+                    else if (!st.empty() && st.top() > 0 && abs(st.top()) > abs(asteroids[i])) {
+                        destroyed = true;
+                    }
+
+                    if (!destroyed) st.push(asteroids[i]);
+                }
+                else if (st.top() <= 0 && asteroids[i] < 0) {
+                    st.push(asteroids[i]);
+                }
+                else if (st.top() < 0 && asteroids[i] > 0) {
                     st.push(asteroids[i]);
                 }
             }
         }
-        while(!st.empty()){
+
+        while (!st.empty()) {
             ans.push_back(st.top());
             st.pop();
         }
