e0aaf6406fe28192d1567447fc3fa3d1ebd741ce
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-10-23 15:55:07 +0530

    Time: 3 ms (87.54%), Space: 10.1 MB (49.83%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -4,19 +4,19 @@ public:
         if(s.size() == 2 && s[0] == s[1]) return true;
         if(s.size() == 2 && s[0] != s[1]) return false;
 
-            set<char>st;
+            // set<char>st;
         while(s.size() != 2){
             string temp;
             for(int i = 0 ; i< s.size() -1 ; i++){
                 int sum = (s[i] - '0') + (s[i+1] - '0');
                 int rem = sum % 10;
                 temp.push_back(rem + '0');
-                st.insert(rem);
+                // st.insert(rem);
             }
-            if(st.size() == 1) return true;
-            st.clear();
+            // if(st.size() == 1) return true;
+            // st.clear();
             s = temp;
         }
-        return false;
+        return s[0] == s[1];
     }
 };
