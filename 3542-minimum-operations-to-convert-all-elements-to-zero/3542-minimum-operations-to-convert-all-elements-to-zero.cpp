e7eb0a33b53906253b761d06c9495ada32a15fd9
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-11-10 21:59:33 +0530

    Time: 35 ms (69.66%), Space: 222 MB (87.36%) - LeetHub

---
+++ 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        stack<int>st;
+        int n = nums.size();
+        int count = 0;
+      
+        for(int i = 0 ; i<  n ; i++){
+            while(!st.empty() && st.top() > nums[i]){
+                st.pop();
+            }
+            
+            if(nums[i] == 0 ) continue;
+            if(st.empty() && nums[i] != 0 ){
+                st.push(nums[i]);
+                count++;
+               
+            } 
+            else if(st.empty() || st.top() < nums[i]){
+                st.push(nums[i]);
+                count++;
+            }
+            
+        }
+        return count;
+    }
+};
